598ee908129b8b3a96290d1c11025dd78003bd01
Veranis Nuñez <Veranisnb@gmail.com>
2022-04-23 16:37:26 -0500

    tabla

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Gestión de estudiantes</h1>
+      <Home />
     </div>
   );
 }
 
 export default App;
-
-
-
-// Aplicación web en react que permita contar con la Creación, Eliminación, Listado y Edición de elementos.
-// El elemento debe contener mínimo 7 campos al momento de crearlos y editarlos.
-// Conectar la aplicación con una base de datos en Firebase
-// Subir la aplicación a AWS Amplify y a Netlify
-// Validar todos los campos
-// Subir en la raíz del proyecto un archivo llamado links.txt que contenga el link de amplify y el de Netlify
-// El proyecto debe tener mínimo 5 commits
-// Dos ramas develop y main
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,52 @@
+import "./styles.css" 
+
+//-- Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPen } from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
 export default function Home() {
-  
+
+  return(
+    <div className="container-fluid p-5">
+      <div className="header mb-5">
+        <h2 className="fw-bolder m-0">GESTIÓN DE ESTUDIANTES</h2>
+        <button className="btn btn-success">
+          Agregar estudiante
+        </button>
+      </div>
+
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Identificación</th>
+            <th scope="col">Teléfono</th>
+            <th scope="col">Email</th>
+            <th scope="col">Carrera</th>
+            <th scope="col">Semestre</th>
+            <th scope="col">Gestión</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Barrios</td>
+            <td>100187527385</td>
+            <td>3132424</td>
+            <td>mark@gmail.com</td>
+            <td>Ing. sistemas</td>
+            <td>9</td>
+            <td>
+              <FontAwesomeIcon icon={faUserPen} className="pe-2 fs-5 text-primary" title="Editar" />
+              <FontAwesomeIcon icon={faTrashCan} className="fs-5 text-danger" title="Eliminar"/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
