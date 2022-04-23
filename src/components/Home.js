11e05043af54875a2875804af32e4236719dc777
Veranis Nuñez <Veranisnb@gmail.com>
2022-04-23 16:53:00 -0500

    formulario agregar estudiante

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,88 @@ import "./styles.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserPen } from '@fortawesome/free-solid-svg-icons'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react"
 
 export default function Home() {
+
+  let [stateAdd, setStateAdd] = useState(false)
 
   return(
     <div className="container-fluid p-5">
       <div className="header mb-5">
         <h2 className="fw-bolder m-0">GESTIÓN DE ESTUDIANTES</h2>
-        <button className="btn btn-success">
-          Agregar estudiante
-        </button>
+        {stateAdd ?
+          <button className="btn btn-danger" onClick={()=>setStateAdd(!stateAdd)} >
+            Cancelar
+          </button>
+          :
+          <button className="btn btn-success" onClick={()=>setStateAdd(!stateAdd)}>
+            Agregar estudiante
+          </button>
+        }
       </div>
 
+      {stateAdd &&
+        <div className="conteiner-add mb-5">
+          <form>
+            <div class="row g-3 input-group mb-3">
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faUser}/>
+                </span>
+                <input type="text" class="form-control" placeholder="Nombre" required/>
+              </div>
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faUser}/>
+                </span>
+                <input type="text" class="form-control" placeholder="Apellido" required/>
+              </div>
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faAddressCard}/>
+                </span>
+                <input type="text" class="form-control" placeholder="Identificación" required/>
+              </div>
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faPhoneFlip}/>
+                </span>
+                <input type="number" class="form-control" placeholder="Teléfono" required/>
+              </div>
+            </div>
+
+            <div class="row g-3 input-group mb-3">
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faEnvelope}/>
+                </span>
+                <input type="email" class="form-control" placeholder="Email" required/>
+              </div>
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faUserGraduate}/>
+                </span>
+                <input type="text" class="form-control" placeholder="Carrera" required/>
+              </div>
+              <div className="col-md d-flex">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faUserGraduate}/>
+                </span>
+                <input type="text" class="form-control" placeholder="Semestre" required/>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-success">
+              Agregar
+            </button>
+          </form>
+        </div>
+      }
       <table class="table table-hover">
         <thead>
           <tr>
@@ -41,8 +111,8 @@ export default function Home() {
             <td>Ing. sistemas</td>
             <td>9</td>
             <td>
-              <FontAwesomeIcon icon={faUserPen} className="pe-2 fs-5 text-primary" title="Editar" />
-              <FontAwesomeIcon icon={faTrashCan} className="fs-5 text-danger" title="Eliminar"/>
+              <FontAwesomeIcon icon={faUserPen} type="button" className="pe-2 fs-5 text-primary" title="Editar" />
+              <FontAwesomeIcon icon={faTrashCan} type="button" className="fs-5 text-danger" title="Eliminar"/>
             </td>
           </tr>
         </tbody>
